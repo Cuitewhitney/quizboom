20ee2b6ec7edfbd68c15ce3932f1751fca6c77bf
Cuitewhitney <118156156+Cuitewhitney@users.noreply.github.com>
2025-12-02 23:18:18 +0000

    root correction

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Fixes asset paths on Render static sites
+  base: '/',  // Fixes asset paths on Render static sites
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
