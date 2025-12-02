c0e7685bd50df1222b44b5b86d87d58a137d1a11
Cuitewhitney <118156156+Cuitewhitney@users.noreply.github.com>
2025-12-02 02:20:42 +0100

    Change socket connection
    Updated socket connection to use production URL and specified transports.

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,8 +1,12 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
+// const socket = io('http://localhost:3000', {
+//   autoConnect: true,
+//   reconnection: true,
+// });
+const socket = io('https://quizboom-backend.onrender.com', {
   autoConnect: true,
-  reconnection: true,
+  transports: ['websocket']  
 });
 
 console.log('Socket created and auto-connected'); // ← you will see this in browser console
