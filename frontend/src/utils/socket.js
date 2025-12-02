f1a975a5d5f5a575a2824cd2f40aeb3386de51eb
Cuitewhitney <118156156+Cuitewhitney@users.noreply.github.com>
2025-12-02 02:34:39 +0100

    Update socket connection URL to production

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,14 +1,8 @@
 import { io } from 'socket.io-client';
 
-// const socket = io('http://localhost:3000', {
-//   autoConnect: true,
-//   reconnection: true,
-// });
 const socket = io('https://quizboom-backend.onrender.com', {
   autoConnect: true,
-  transports: ['websocket']  
+  transports: ['websocket']   
 });
-
-console.log('Socket created and auto-connected'); // ← you will see this in browser console
 
 export default socket;
